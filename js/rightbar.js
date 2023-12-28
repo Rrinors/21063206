88b938d42261e26545c180724a527af33952ef61
Rinors <13641064+rinors@user.noreply.gitee.com>
2023-12-29 00:48:41 +0800

    修改右边栏定位逻辑

--- js/rightbar.js
+++ js/rightbar.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var headings = document.querySelectorAll('pos');
+    var headings = document.querySelectorAll('h1');
   
     var rightbar = document.getElementById('rightbar');
   
     headings.forEach(function(heading) {
       var link = document.createElement('a');
-      link.textContent = heading.id;
+      link.textContent = heading.textContent;
       link.href = '#' + heading.id;
   
       // 将链接添加到 sidebar 中
