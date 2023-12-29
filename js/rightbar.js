9c44d066157c8ca7e5325a113fd919f9a4309e7b
Rinors <13641064+rinors@user.noreply.gitee.com>
2023-12-29 15:43:21 +0800

    修改注释

--- js/rightbar.js
+++ js/rightbar.js
@@ -1,24 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var headings = document.querySelectorAll('h1');
-  
-    var rightbar = document.getElementById('rightbar');
-  
-    headings.forEach(function(heading) {
-      var link = document.createElement('a');
-      link.textContent = heading.textContent;
-      link.href = '#' + heading.id;
-  
-      // 将链接添加到 sidebar 中
-      rightbar.appendChild(link);
-  
-      // 为链接添加点击事件监听器，实现页内跳转
-      link.addEventListener('click', function(event) {
-        event.preventDefault();
-  
-        // 使用 smooth 滚动效果
-        document.querySelector(link.getAttribute('href')).scrollIntoView({
-          behavior: 'smooth',
-        });
+  var headings = document.querySelectorAll('h1');
+
+  var rightbar = document.getElementById('rightbar');
+
+  headings.forEach(function(heading) {
+    var link = document.createElement('a');
+    link.textContent = heading.textContent;
+    link.href = '#' + heading.id;
+
+    rightbar.appendChild(link);
+
+    link.addEventListener('click', function(event) {
+      event.preventDefault();
+
+      document.querySelector(link.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth',
       });
     });
   });
+});
